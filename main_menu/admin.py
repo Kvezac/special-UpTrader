c9b8d6b597ef114b61b87106275282119defe3f7
Kvezac <Kvezak@gmail.com>
2023-11-06 09:23:46 +0300

    modified:   .idea/workspace.xml 	modified:   main_menu/admin.py 	modified:   main_menu/models.py 	modified:   main_menu/templatetags/draw_menu.py 	modified:   templates/tags/menu_tag.html

--- main_menu/admin.py
+++ main_menu/admin.py
@@ -1,20 +1,7 @@
 from django.contrib import admin
-from .models import Menu, Item
+from .models import Menu
 
 
 @admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("title",)}
-
-
-@admin.register(Item)
-class MenuItemAdmin(admin.ModelAdmin):
-    list_filter = ("menu",)
-    fieldsets = (
-        ("Add new item", {
-            "description": "Parent should be a menu or item",
-            "fields": (("menu", "parent"), "title", "slug")
-        }),
-    )
-    # readonly_fields = ["slug"]
-    prepopulated_fields = {"slug": ("title",)}
+    prepopulated_fields = {"slug": ("name",)}
